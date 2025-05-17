469361bbfc40bde344ef5ef82925ce6d81c023ad
HelenPhamius <hcuterabbit@gmail.com>
2025-05-16 19:27:09 -0700

    Image and new screen
    I can change the pic. It is TEMMIE. I can also make a new screen and go back and forth between the two through buttons and onEvents.

--- app/src/main/java/com/example/trial3/MainActivity2.java
+++ app/src/main/java/com/example/trial3/MainActivity2.java
@@ -3,7 +3,6 @@ package com.example.trial3;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,13 +10,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -25,14 +24,7 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public void tryToast(View view){
-        Toast toast = Toast.makeText(this, "Button clicked", Toast.LENGTH_LONG);
-        toast.show();
+    public void back(View view){
+        startActivity(new Intent(MainActivity2.this, MainActivity.class));
     }
-
-    public void forward(View view){
-        startActivity(new Intent(MainActivity.this, MainActivity2.class));
-    }
-
-
 }
