9abd54b386260aea4a39c7619875db973832bd60
HelenPhamius <hcuterabbit@gmail.com>
2025-06-08 16:17:07 -0700

    Navigation officially working

--- app/src/main/java/com/example/trial3/MainActivity3.java
+++ app/src/main/java/com/example/trial3/MainActivity3.java
@@ -1,6 +1,9 @@
 package com.example.trial3;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +23,12 @@ public class MainActivity3 extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void home(View view){
+        startActivity(new Intent(MainActivity3.this, MainActivity.class));
+    }
+    public void backFrom3(View view){
+        startActivity(new Intent(MainActivity3.this, MainActivity2.class));
     }
 }
