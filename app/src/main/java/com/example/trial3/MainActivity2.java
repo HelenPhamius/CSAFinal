8228ce1c7e96afd921a057cb9964387e701ad2fc
HelenPhamius <hcuterabbit@gmail.com>
2025-06-08 16:39:30 -0700

    Yay I can change text

--- app/src/main/java/com/example/trial3/MainActivity2.java
+++ app/src/main/java/com/example/trial3/MainActivity2.java
@@ -3,6 +3,7 @@ package com.example.trial3;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,7 +12,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity2 extends AppCompatActivity {
-
+    private TextView tV;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +23,8 @@ public class MainActivity2 extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        tV = findViewById(R.id.textView);
+
     }
 
     public void back(View view){
@@ -30,5 +33,9 @@ public class MainActivity2 extends AppCompatActivity {
 
     public void next(View view){
         startActivity(new Intent(MainActivity2.this, MainActivity3.class));
+    }
+
+    public void decRating(View view){
+            tV.setText("hoi");
     }
 }
