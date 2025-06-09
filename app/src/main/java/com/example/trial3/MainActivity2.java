c418322bfdeca4ec88cbf521bfe1c98ee98def75
HelenPhamius <hcuterabbit@gmail.com>
2025-06-08 17:44:02 -0700

    The user can move between the imgs now

--- app/src/main/java/com/example/trial3/MainActivity2.java
+++ app/src/main/java/com/example/trial3/MainActivity2.java
@@ -3,6 +3,7 @@ package com.example.trial3;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -13,7 +14,10 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity2 extends AppCompatActivity {
     private TextView ratingTV;
+    private ImageView catImg;
+    private int[] imgList = {R.drawable.cute, R.drawable.mello, R.drawable.mocha, R.drawable.po, R.drawable.sema };
     private int rate = 0;
+    private int index = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,7 +29,9 @@ public class MainActivity2 extends AppCompatActivity {
             return insets;
         });
         ratingTV = findViewById(R.id.textView);
+        catImg = findViewById(R.id.imageView2);
 
+        catImg.setImageResource(imgList[0]);
     }
 
     public void back(View view){
@@ -44,5 +50,15 @@ public class MainActivity2 extends AppCompatActivity {
     public void incRating(View view){
         rate++;
         ratingTV.setText(rate + "/10");
+    }
+
+    public void backImg(View view){
+        index--;
+        catImg.setImageResource(imgList[index]);
+    }
+
+    public void nextImg(View view){
+        index++;
+        catImg.setImageResource(imgList[index]);
     }
 }
